034fa5bbf6eb07b6da5cb78656fdf548866ef20d
Sebastian Dorn <sebastian.dorn@virtual7.de>
2018-11-15 11:52:11 +0100

    Virtuelles Scrolling Beispiel hinzugefügt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'angular-virtual-scrolling';
+  items = Array.from({length: 1000000})
+    .map((_, item) => (item * 16).toString(16))
+    .map((item: string) => '#' + ('000000' + item).substr(item.length).toUpperCase());
 }
